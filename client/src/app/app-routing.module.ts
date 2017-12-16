cb17337b6f40ab9e08d471f51c7272baa5e7d57c
abhijeet <ab.progr@gmail.com>
2017-12-16 17:38:00 +0530

    routes

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: HomeComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
